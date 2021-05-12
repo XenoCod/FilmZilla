5cd39695da43f76d89428acdec2369e379294f68
ADITYA <adi@gmail.com>
2021-05-12 18:42:20 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "image.tmdb.org",
+      "icons8.com",
+      "img.icons8.com",
+    ],
+  },
+};
+
+// specifying all the domains from where we will be using the images importing
+//This is strictly followed
